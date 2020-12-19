489d9e384f4f744c04e906c2d15f29a9da8ad47b
Akash Dubey <akashrdubey19@gmail.com>
2020-12-19 21:38:32 +0530

    code deployed on heroku with backend

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log("Server started!")
+    console.log("Server started!!")
 });
